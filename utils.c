a158f3b0cb509be8fffe82359ffc6f129946d7e8
David Cruz García <david.cruz.garciaa@gmail.com>
2022-02-07 00:46:45 +0100

    Added SHA256 hashing and ZLIB (de)compression

--- utils.c
+++ utils.c
@@ -92,3 +92,177 @@ create_file(char *path)
 	return 0;	
 }
 
+
+
+/**
+ * Calculates sha256 hash of a given file.
+ */ 
+int 
+sha256(char *file, unsigned char **sha256hash){
+	size_t len;
+	unsigned char chunk[SHA256_DIGEST_LENGTH];	
+	unsigned char hash[SHA256_DIGEST_LENGTH]  = { 0 };
+	
+	FILE *fd = fopen(file, "rb");
+	if(fd == NULL)
+		return -1;
+	
+	SHA256_CTX ctx;
+  	SHA256_Init(&ctx);
+  	while (len = fread(chunk, 1, SHA256_DIGEST_LENGTH, fd)) {
+		SHA256_Update(&ctx, chunk, len);
+  	}
+  	SHA256_Final(hash, &ctx);
+	
+	fclose(fd);
+	
+	//Reserve 65 bytes for the string: 64 bytes (256 bits * 2) for the hash in 
+	// hexadecimal (2 hex digits for each of the 32 bytes of the hash), and the 
+	// last for '\0'
+	(*sha256hash) = malloc((2*SHA256_DIGEST_LENGTH + 1) * sizeof(unsigned char));
+	memset(*sha256hash, '\0', 65);
+
+	for(int i=0; i<SHA256_DIGEST_LENGTH; i++){
+		sprintf((*sha256hash) + 2*i, "%02x", hash[i]);
+	}   
+	return 0;
+}
+
+
+/**
+ * Compress the file in into file out
+ */
+int 
+compress_file(char *file_in_path, char *file_out_path)
+{
+	FILE *fin = fopen(file_in_path, "rb");
+	FILE *fout = fopen(file_out_path, "wb");
+	
+	if(fin == NULL || fout == NULL)
+		return Z_ERRNO;
+
+
+	z_stream stream = {
+		.zalloc = Z_NULL, 
+		.zfree  = Z_NULL, 
+		.opaque = Z_NULL
+	};
+	int ret, flush;
+	unsigned int have;	
+	uint8_t in[COMPRESS_CHUNK];	
+	uint8_t out[COMPRESS_CHUNK];	
+
+	ret = deflateInit(&stream, Z_DEFAULT_COMPRESSION);	
+	if(ret != Z_OK)
+		return ret;
+
+	do{
+		stream.avail_in = fread(in, 1, COMPRESS_CHUNK, fin);
+		if(ferror(fin)){
+			deflateEnd(&stream);
+			return Z_ERRNO;
+		}
+		
+		if(feof(fin))  flush = Z_FINISH;
+		else              flush = Z_NO_FLUSH;
+			
+		stream.next_in = in;
+		
+		do{	
+			stream.avail_out = COMPRESS_CHUNK;
+			stream.next_out = out;
+			
+			ret = deflate(&stream, flush);
+			assert(ret != Z_STREAM_ERROR);
+
+			have = COMPRESS_CHUNK - stream.avail_out;
+			if(fwrite(out, 1, have, fout) != have || ferror(fout)){
+				deflateEnd(&stream);
+				return Z_ERRNO;
+			}	
+		}while(stream.avail_out == 0);
+		assert(stream.avail_out == 0);
+
+	}while(flush != Z_FINISH);
+	assert(ret == Z_STREAM_END);	
+
+	deflateEnd(&stream);
+	return Z_OK;
+}
+
+
+/**
+ * Decompress the file in into file out
+ */
+int 
+decompress_file(char *file_in_path, char *file_out_path)
+{
+	FILE *fin = fopen(file_in_path, "rb");
+	FILE *fout = fopen(file_out_path, "wb");
+	
+	if(fin == NULL || fout == NULL)
+		return Z_ERRNO;
+
+	z_stream stream = {
+		.zalloc = Z_NULL,
+		.zfree  = Z_NULL,
+		.opaque  = Z_NULL,
+		.avail_in = 0,
+		.next_in = Z_NULL	
+	};	
+		
+	int ret;
+	unsigned int have;	
+	uint8_t in[COMPRESS_CHUNK];	
+	uint8_t out[COMPRESS_CHUNK];	
+
+	ret = inflateInit(&stream);
+	if(ret != Z_OK)
+		return ret;
+
+	do{
+		stream.avail_in = fread(in, 1, COMPRESS_CHUNK, fin);
+		if(ferror(fin)){
+			inflateEnd(&stream);
+			return Z_ERRNO;
+		}
+
+		if(stream.avail_in == 0) 
+			break;
+	
+		stream.next_in = in;
+		do{
+			stream.avail_out = COMPRESS_CHUNK;
+			stream.next_out  = out;
+
+			ret = inflate(&stream, Z_NO_FLUSH);
+			assert(ret != Z_STREAM_ERROR);
+
+			switch(ret){
+				case Z_NEED_DICT:
+					ret = Z_DATA_ERROR;
+				case Z_DATA_ERROR:
+				case Z_MEM_ERROR:
+					inflateEnd(&stream);
+					return ret;
+			}
+
+			have = COMPRESS_CHUNK - stream.avail_out;
+			if(fwrite(out, 1, have, fout) != have || ferror(fout)){
+				inflateEnd(&stream);
+				return Z_ERRNO;
+			}
+		}while(stream.avail_out == 0);		
+	}while(ret != Z_STREAM_END);
+
+	inflateEnd(&stream);
+	if(ret == Z_STREAM_END)
+		return Z_OK;
+	else
+		return Z_DATA_ERROR;		
+}
+
+
+
+
+
--- utils.h
+++ utils.h
@@ -3,19 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <assert.h>
+#include <openssl/sha.h>
+#include <zlib.h>
 
-#define PATH_LIMIT 100
+
+#define PATH_LIMIT 200
 #define CONF_LIMIT 100
 
 #define ARRAYLEN(array) (sizeof(array)/sizeof(array[0]))
 #define STRIP(s) if(s[strlen(s)-1] == '\n') s[strlen(s)-1] = '\0';
 
+#define COMPRESS_CHUNK 16384
+
+//Paths, files and directories
 int   get_actual_path(char *actual_path);
 char* create_path(char *path, char *subpath);
 int   dir_exists(char *path);
@@ -23,5 +31,14 @@ int   create_dir(char *path);
 int   file_exists(char *path);
 int   create_file(char *path);
 
+
+//Encryption
+int sha256(char *file, unsigned char **sha256hash);
+
+
+//Compression/Decompression
+//Based on example in: https://zlib.net/zpipe.c
+int compress_file(char *file_in_path, char *file_out_path);
+int decompress_file(char *file_in_path, char *file_out_path);
 
 #endif
